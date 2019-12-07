eacfa71fa8567892498d3859ee842634d340e5dc
Kevin Kamps <k.kamps@tfe.nl>
2019-12-07 21:20:37 +0100

    Added release script

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,9 @@
-FROM golang:latest AS build-env
-RUN export GO111MODULE=on
-RUN mkdir -p /app
-WORKDIR /app
-COPY . ./
-RUN GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$SOURCE_BRANCH" -o /app/registrar_linux_amd64 ./main.go
-
-
-VOLUME /app
-
-
 FROM busybox:1.30-glibc
 
 LABEL maintainer="Kevin Kamps"
 LABEL github="https://github.com/kevinkamps/Registrar"
 LABEL license="GPL-3.0"
 
-COPY --from=build-env /app/registrar_linux_amd64 /bin/registrar
+COPY bin/registrar_linux_amd64 /bin/registrar
 
 ENTRYPOINT ["/bin/registrar"]
---
+++ release.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo -e "Version: "
+read VERSION
+
+export GO111MODULE=on
+
+mkdir bin
+rm -rf bin/*
+
+echo "$(date): Building for windows"
+GOOS=windows GOARCH=386 go build -ldflags "-X main.version=$VERSION" -o ./bin/registrar_386.exe ./main.go
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -o ./bin/registrar_amd64.exe ./main.go
+
+echo "$(date): Building for Linux"
+GOOS=linux GOARCH=386 go build -ldflags "-X main.version=$VERSION" -o ./bin/registrar_linux_386 ./main.go
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -o ./bin/registrar_linux_amd64 ./main.go
+
+echo "$(date): Building for Linux arm"
+GOOS=linux GOARCH=arm go build -ldflags "-X main.version=$VERSION" -o ./bin/registrar_linux_arm ./main.go
+
+echo "$(date): Building container"
+docker build -t kevinkamps/registrar:$VERSION .
+
+echo "$(date): Pushing to docker"
+echo -e "Want to push to Docker (y/n)"
+read CHOICE
+if [ $CHOICE = y ]
+	then
+	docker push kevinkamps/registrar:$VERSION
+fi
+
+
+echo "$(date): Done"
